1d6052432a86b407e0b7ff74c4ce9fea63099807
lexxxxa82 <lexxxxa.82@mail.ru>
2022-12-16 23:45:55 +0800

    1

--- src/Car.java
+++ src/Car.java
@@ -15,33 +15,51 @@ public class Car {
     }
 
     public String getBrand() {
+        if (brand == null || brand.isEmpty()) {
+            brand = "default";
+        }
         return brand;
     }
 
     public String getModel() {
+        if (model == null || model.isEmpty()) {
+            brand = "default";
+        }
         return model;
     }
 
     public double getEngineVolume() {
+        if (engineVolume <= 0) {
+            engineVolume = 1.5;
+        }
         return engineVolume;
     }
 
     public String getColor() {
+        if (color == null || color.isEmpty()) {
+            color = " белый";
+        }
         return color;
     }
 
     public int getYear() {
+            if (year <= 0) {
+                year = 2000;
+        }
         return year;
     }
 
     public String getCountry() {
+        if (country == null || country.isEmpty()) {
+            country = "default";
+        }
         return country;
     }
 
     @Override
     public String toString() {
-        return " марка "+getBrand()+" "+getModel()+"год выпуска "+getYear()+ "сборка в "+ getCountry()+" "+getColor()+
-                " цвета, объем двигателя"+getEngineVolume()+" л.";
+        return " марка: "+getBrand()+", модель: "+getModel()+", год выпуска "+getYear()+ ", сборка в "+ getCountry()+", "+getColor()+
+                " цвет, объем двигателя "+getEngineVolume()+" л.";
     }
 }
 
